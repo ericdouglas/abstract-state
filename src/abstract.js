84a577ff20e7fbfacaf0811f6b70051887441dda
Eric Douglas <eric.douglas.mail@gmail.com>
2020-06-21 14:01:45 -0300

    docs: abstract

--- src/abstract.js
+++ src/abstract.js
@@ -3,7 +3,16 @@ import { createContext } from 'react'
 import createContextHook from './create-context-hook'
 import ProviderFactory from './provider-factory'
 
-const abstract = (actions = {}, initialState, moduleName = '') => {
+/**
+ * Generate two providers (state and actions) and two custom hooks to connect to them
+ *
+ * @param {Object} actions
+ * @param {*} initialState
+ * @param {string} moduleName
+ * @returns {{ReactNode, () => void, () => void}} returns a Provider and two custom hooks
+ * Name of custom hooks will be based on the `moduleName` passed to the abstract function
+ */
+const abstract = (actions, initialState, moduleName) => {
   const useModuleStateName = `use${moduleName}State`
   const useModuleActionsName = `use${moduleName}Actions`
   const providerName = `${moduleName}Provider`
