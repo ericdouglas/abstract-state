13b271cf54c0ef629d28f5a5a4c1e6d5a5068deb
Eric Douglas <eric.douglas.mail@gmail.com>
2020-06-21 19:30:35 -0300

    refactor: return to previous version with factory

--- src/abstract.js
+++ src/abstract.js
@@ -1,50 +1,18 @@
-// import { createContext } from 'react'
-
-// import createContextHook from './create-context-hook'
-// import ProviderFactory from './provider-factory'
-
-// /**
-//  * Generate two providers (state and actions) and two custom hooks to connect to them
-//  *
-//  * @param {Object} actions
-//  * @param {*} initialState
-//  * @param {string} moduleName
-//  * @returns {{ ReactNode, () => any, () => any }} returns a Provider and two custom hooks
-//  * Name of custom hooks will be based on the `moduleName` passed to the abstract function
-//  */
-// const abstract = (actions, initialState, moduleName) => {
-//   const useModuleStateName = `use${moduleName}State`
-//   const useModuleActionsName = `use${moduleName}Actions`
-//   const providerName = `${moduleName}Provider`
-
-//   const StateContext = createContext({})
-//   const ActionsContext = createContext({})
-
-//   const Provider = ProviderFactory(
-//     actions,
-//     initialState,
-//     StateContext,
-//     ActionsContext,
-//   )
-
-//   return {
-//     Provider,
-//     [useModuleStateName]: () =>
-//       createContextHook(useModuleStateName, providerName, StateContext),
-//     [useModuleActionsName]: () =>
-//       createContextHook(useModuleActionsName, providerName, ActionsContext),
-//   }
-// }
-
-// export default abstract
-
-import React, { createContext } from 'react'
-import onli, { onliSend } from 'onli-reducer'
-import { useImmerReducer } from 'use-immer'
+import { createContext } from 'react'
 
 import createContextHook from './create-context-hook'
+import ProviderFactory from './provider-factory'
 
-const abstract = (actions = {}, initialState, moduleName = '') => {
+/**
+ * Generate two providers (state and actions) and two custom hooks to connect to them
+ *
+ * @param {Object} actions
+ * @param {*} initialState
+ * @param {string} moduleName
+ * @returns {{ ReactNode, () => any, () => any }} returns a Provider and two custom hooks
+ * Name of custom hooks will be based on the `moduleName` passed to the abstract function
+ */
+const abstract = (actions, initialState, moduleName) => {
   const useModuleStateName = `use${moduleName}State`
   const useModuleActionsName = `use${moduleName}Actions`
   const providerName = `${moduleName}Provider`
@@ -52,20 +20,12 @@ const abstract = (actions = {}, initialState, moduleName = '') => {
   const StateContext = createContext({})
   const ActionsContext = createContext({})
 
-  // eslint-disable-next-line react/prop-types
-  const Provider = ({ children }) => {
-    const [reducer, types] = onli(actions)
-    const [state, dispatch] = useImmerReducer(reducer, initialState)
-    const send = onliSend(dispatch, types)
-
-    return (
-      <StateContext.Provider value={state}>
-        <ActionsContext.Provider value={send}>
-          {children}
-        </ActionsContext.Provider>
-      </StateContext.Provider>
-    )
-  }
+  const Provider = ProviderFactory(
+    actions,
+    initialState,
+    StateContext,
+    ActionsContext,
+  )
 
   return {
     Provider,
--- src/abstract.test.js
+++ src/abstract.test.js
@@ -1,23 +1,20 @@
-// import React from 'react'
-// import { renderHook } from '@testing-library/react-hooks'
-
 import abstract from './abstract'
 
 const increment = (state, value) => state + value
 const decrement = (state, value) => state - value
 
 const actions = { increment, decrement }
-const initialState = 0
+const initialState = 10
 const moduleName = 'Test'
 
 describe('abstract', () => {
-  it('should return a provider and two custom hooks', () => {
-    const { Provider, useTestState, useTestActions } = abstract(
-      actions,
-      initialState,
-      moduleName,
-    )
+  const { Provider, useTestState, useTestActions } = abstract(
+    actions,
+    initialState,
+    moduleName,
+  )
 
+  it('should return a provider and two custom hooks', () => {
     expect(Provider).toBeTruthy()
 
     expect(useTestActions).toBeTruthy()
@@ -26,17 +23,4 @@ describe('abstract', () => {
     expect(useTestState).toBeTruthy()
     expect(typeof useTestState).toBe('function')
   })
-
-  // it('should return the current state when using the custom hook', () => {
-  //   const { Provider, useTestState } = abstract(
-  //     actions,
-  //     initialState,
-  //     moduleName,
-  //   )
-  //   const wrapper = ({ children }) => <Provider>{children}</Provider>
-  //   const { result } = renderHook(() => useTestState(), { wrapper })
-  //   console.log('result.current :::', result.current)
-
-  //   expect(result.current).toBe(0)
-  // })
 })
