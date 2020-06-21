3c147f3bd575bbb6ed554644f76d62bb67dfe13e
Eric Douglas <eric.douglas.mail@gmail.com>
2020-06-21 18:42:19 -0300

    fix: check crazy bug

--- src/abstract.js
+++ src/abstract.js
@@ -1,20 +1,50 @@
+// import { createContext } from 'react'
+
+// import createContextHook from './create-context-hook'
+// import ProviderFactory from './provider-factory'
+
+// /**
+//  * Generate two providers (state and actions) and two custom hooks to connect to them
+//  *
+//  * @param {Object} actions
+//  * @param {*} initialState
+//  * @param {string} moduleName
+//  * @returns {{ ReactNode, () => any, () => any }} returns a Provider and two custom hooks
+//  * Name of custom hooks will be based on the `moduleName` passed to the abstract function
+//  */
+// const abstract = (actions, initialState, moduleName) => {
+//   const useModuleStateName = `use${moduleName}State`
+//   const useModuleActionsName = `use${moduleName}Actions`
+//   const providerName = `${moduleName}Provider`
+
+//   const StateContext = createContext({})
+//   const ActionsContext = createContext({})
+
+//   const Provider = ProviderFactory(
+//     actions,
+//     initialState,
+//     StateContext,
+//     ActionsContext,
+//   )
+
+//   return {
+//     Provider,
+//     [useModuleStateName]: () =>
+//       createContextHook(useModuleStateName, providerName, StateContext),
+//     [useModuleActionsName]: () =>
+//       createContextHook(useModuleActionsName, providerName, ActionsContext),
+//   }
+// }
+
+// export default abstract
+
 import React, { createContext } from 'react'
 import onli, { onliSend } from 'onli-reducer'
 import { useImmerReducer } from 'use-immer'
 
 import createContextHook from './create-context-hook'
-// import ProviderFactory from './provider-factory'
 
-/**
- * Generate two providers (state and actions) and two custom hooks to connect to them
- *
- * @param {Object} actions
- * @param {*} initialState
- * @param {string} moduleName
- * @returns {{ ReactNode, () => any, () => any }} returns a Provider and two custom hooks
- * Name of custom hooks will be based on the `moduleName` passed to the abstract function
- */
-const abstract = (actions, initialState, moduleName) => {
+const abstract = (actions = {}, initialState, moduleName = '') => {
   const useModuleStateName = `use${moduleName}State`
   const useModuleActionsName = `use${moduleName}Actions`
   const providerName = `${moduleName}Provider`
@@ -36,13 +66,6 @@ const abstract = (actions, initialState, moduleName) => {
       </StateContext.Provider>
     )
   }
-
-  // const Provider = ProviderFactory(
-  //   actions,
-  //   initialState,
-  //   StateContext,
-  //   ActionsContext,
-  // )
 
   return {
     Provider,
